1b580aa65b1dc36b52a0e2a82818cd0859915bf0
VladimirIvanov <ivanovvladimir311992@gmail.com>
2022-09-23 22:04:02 +0300

    Tasks 1,2 added

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Task 1 with string
+void Task1()
+{   
+    Console.WriteLine("Enter three  digits number");
+    string numbers = Console.ReadLine();
+    Console.WriteLine(numbers[1]);
+
+}
+//Task1 with int
+
+void Task2()
+{
+    Console.WriteLine("Enter three  digits number");
+    int numbers = int.Parse(Console.ReadLine());
+    Console.WriteLine((numbers/10)%10);
+}
+
+//Task 2
+void Task3()
+{
+    Console.WriteLine("Enter three digits number");
+    int numbers = int.Parse(Console.ReadLine());
+    string numberstext = Convert.ToString(numbers);
+    if (numberstext.Length > 2)
+    {
+        Console.WriteLine("Third number is " + numberstext[2]);
+    }
+    else {
+        Console.WriteLine("Theres no third number");
+    }
+}
+Task3();
+//Task3
+void Task4()
+{
+    clnsole.WriteLine("Enter day number");
+}
